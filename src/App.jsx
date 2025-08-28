06973e69ed4dd22ddcf8f4e965920a330f4173dd
Shikhar Srivastav <shikharsrivastav9450@gmail.com>
2025-08-28 23:28:26 +0530

    second class

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,27 @@
 import { createBrowserRouter } from "react-router";
 import { RouterProvider } from "react-router/dom";
 import { Home } from "./components/Home";
+import About from "./components/About";
+import Layout from "./components/Layout";
+
 
 function App() {
 
   const router = createBrowserRouter([
     {
       path: "/",
-      element: <Home />
-    },
+      element: <Layout/>,
+      children: [
+        {
+          path: "/",
+          element: <Home/>
+        },
+        {
+          path: "About",
+          element: <About/>
+        }
+      ]
+    }
   ]);
 
   return (
